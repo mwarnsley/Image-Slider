ce1b377a56ea5d6c7e4428b054afb3469420ad5e
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-01-29 10:46:04 -0500

    initial setup of javascript to give the slider functionality

--- slider.js
+++ slider.js
@@ -1,1 +1,7 @@
-ECHO is on.
+$(document).ready(function(){
+    (function(){
+        setInterval(function(){
+            $("#slider .slides").animate({'margin-left': '-=720px'}, 1000);
+        }, 3000);
+    })();
+});
