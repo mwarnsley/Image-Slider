122f405466b0c85a5861a5dd68e049befe9a2574
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-01-29 10:51:22 -0500

    editing configurations for the interval function and storing things in variables

--- slider.js
+++ slider.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
     (function(){
+
+        //configuration for variables in terms of width, animation speed, and pausing time of the slider
+        var width = 720;
+        var animationSpeed = 1000;
+        var pause = 3000;
+
+        //set interval function to animate for the slider speed and pausing
         setInterval(function(){
-            $("#slider .slides").animate({'margin-left': '-=720px'}, 1000);
-        }, 3000);
+            $("#slider .slides").animate({'margin-left': '-=' + width}, animationSpeed);
+        }, pause);
     })();
 });
