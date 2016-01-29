2730a7681a5ed09331f35da0920224ce58ba212c
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-01-29 11:45:01 -0500

    adding the pause and start for mousing over the slider

--- slider.js
+++ slider.js
@@ -7,21 +7,33 @@ $(document).ready(function(){
         var pause = 1000;
         var currentSlide = 1;
 
-        //caching the dom
+        //caching the dom by setting variables
         var $slider = $("#slider");
         var $slideContainer = $slider.find(".slides");
         var $slides = $slideContainer.find(".slide");
+        var interval;
 
-        //set interval function to animate for the slider speed and pausing
-        setInterval(function(){
-            $slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function(){
-                currentSlide++;
-                if(currentSlide === $slides.length){
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
-                }
-            });
-        }, pause);
-        
+        function startSlider(){
+            //set interval function to animate for the slider speed and pausing
+            interval = setInterval(function(){
+                $slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function(){
+                    currentSlide++;
+                    if(currentSlide === $slides.length){
+                        currentSlide = 1;
+                        $slideContainer.css('margin-left', 0);
+                    }
+                });
+            }, pause);
+
+        }
+
+        function pauseSlider(){
+            clearInterval(interval)
+        }
+        //trigger the pause and start for the slider
+        $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+
+        startSlider();
+
     })();
 });
